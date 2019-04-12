67829452d3fb5f302e6ba2d078bf2ce2975527fa
EC2 Default User <ec2-user@ip-172-31-22-63.us-east-2.compute.internal>
2019-04-12 03:28:27 +0000

    device add packages

--- app/models/support_device.rb
+++ app/models/support_device.rb
@@ -1,4 +1,19 @@
 class SupportDevice < ApplicationRecord
   belongs_to :package
   belongs_to :device
+  
+  def SupportDevice.delete_record(device_id)
+    support_devices = SupportDevice.where(device_id: device_id).find_each
+    support_devices.each do |support_device|
+      SupportDevice.destroy(support_device.id)
+    end
+  end
+
+  
+  def SupportDevice.create_record(device_id, items)
+    items.each do |item|
+      SupportDevice.create device_id: device_id, package_id: item
+    end
+  end
+  
 end
